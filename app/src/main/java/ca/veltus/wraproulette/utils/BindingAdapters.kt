26fa006fffaf468291b742aeda124b1463bfadc0
Grayson Ruffo <93240608+g-ruffo@users.noreply.github.com>
2022-10-12 20:21:06 -0700

    feat: Create binding adapter to set helper text
    Created setEmailHelperTextColor function that sets an OnFocusChangeListener to change the color and text of the TextInputLayout.

--- app/src/main/java/ca/veltus/wraproulette/utils/BindingAdapters.kt
+++ app/src/main/java/ca/veltus/wraproulette/utils/BindingAdapters.kt
@@ -1,2 +1,41 @@
 package ca.veltus.wraproulette.utils
 
+import android.content.res.ColorStateList
+import android.util.Patterns
+import androidx.core.content.ContextCompat
+import androidx.databinding.BindingAdapter
+import ca.veltus.wraproulette.R
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+
+object BindingAdapters {
+    @BindingAdapter(
+        value = ["emailHelperTextColorSwitcher", "emailTextInputEditTextStatus"], requireAll = true
+    )
+    @JvmStatic
+    fun setEmailHelperTextColor(view: TextInputLayout, text: String?, editText: TextInputEditText) {
+        val context = view.context
+        editText.setOnFocusChangeListener { v, hasFocus ->
+            if (!Patterns.EMAIL_ADDRESS.matcher(text.toString())
+                    .matches() && !hasFocus && text != "null" && !text.isNullOrEmpty()
+            ) {
+                view.setHelperTextColor(
+                    ColorStateList.valueOf(
+                        ContextCompat.getColor(context, R.color.warningRed)
+                    )
+                )
+                view.helperText = context.getString(R.string.invalidEmailAddressHelper)
+
+            } else {
+                view.setHelperTextColor(
+                    ColorStateList.valueOf(
+                        ContextCompat.getColor(context, R.color.optionalGrey)
+                    )
+                )
+                view.helperText = null
+            }
+        }
+    }
+
+}
+
