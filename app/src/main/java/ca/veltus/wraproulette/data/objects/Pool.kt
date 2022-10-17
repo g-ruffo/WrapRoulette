0b6acc2ca94c9f8691b6e2921713546a0afc4cf8
Grayson Ruffo <93240608+g-ruffo@users.noreply.github.com>
2022-10-17 05:43:17 -0700

    feat: Attach onClickListener to RecyclerView items

--- app/src/main/java/ca/veltus/wraproulette/data/objects/Pool.kt
+++ app/src/main/java/ca/veltus/wraproulette/data/objects/Pool.kt
@@ -1,12 +1,11 @@
 package ca.veltus.wraproulette.data.objects
 
-import android.os.Parcelable
+import android.view.View
 import ca.veltus.wraproulette.R
 import ca.veltus.wraproulette.databinding.PoolListItemBinding
+import ca.veltus.wraproulette.utils.FirestoreUtil
 import com.google.firebase.Timestamp
 import com.xwray.groupie.databinding.BindableItem
-import kotlinx.parcelize.Parcelize
-import java.util.Date
 
 data class Pool(
     var docId: String,
@@ -30,6 +29,20 @@ class PoolItem(val pool: Pool
 
     override fun bind(viewBinding: PoolListItemBinding, position: Int) {
         viewBinding.pool = pool
+        FirestoreUtil.getCurrentUser { user ->
+            if (pool.docId == user.activePool) {
+                viewBinding.selectedPoolIcon.visibility = View.VISIBLE
+
+            } else {
+                viewBinding.selectedPoolIcon.visibility = View.GONE
+            }
+            user.activePool
+        }
+        viewBinding.cardView.setOnClickListener {
+            FirestoreUtil.setActivePool(pool.docId) {
+                viewBinding.selectedPoolIcon.visibility = View.VISIBLE
+            }
+        }
     }
 
     override fun getLayout(): Int {
