9f00e1ab39a2440c4ae04f004c64533a8311b5ff
Grayson Ruffo <93240608+g-ruffo@users.noreply.github.com>
2022-10-16 18:49:22 -0700

    feat: Create expandable fab button for fragment_pools.xml

--- app/src/main/java/ca/veltus/wraproulette/ui/pools/PoolsFragment.kt
+++ app/src/main/java/ca/veltus/wraproulette/ui/pools/PoolsFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -30,12 +32,39 @@ class PoolsFragment : BaseFragment() {
     override val _viewModel by viewModels<PoolsViewModel>()
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var databaseReference: FirebaseFirestore
+    private val rotateOpen: Animation by lazy {
+        AnimationUtils.loadAnimation(
+            requireContext(),
+            R.anim.rotate_open_anim
+        )
+    }
+    private val rotateClose: Animation by lazy {
+        AnimationUtils.loadAnimation(
+            requireContext(),
+            R.anim.rotate_close_anim
+        )
+    }
+    private val fromBottom: Animation by lazy {
+        AnimationUtils.loadAnimation(
+            requireContext(),
+            R.anim.from_bottom_anim
+        )
+    }
+    private val toBottom: Animation by lazy {
+        AnimationUtils.loadAnimation(
+            requireContext(),
+            R.anim.to_bottom_anim
+        )
+    }
+    private var fabClicked = false
+
 
     private var shouldInitRecyclerView = true
     private lateinit var poolListenerRegistration: ListenerRegistration
     private lateinit var poolSection: Section
 
     private var _binding: FragmentPoolsBinding? = null
+
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
@@ -51,20 +80,70 @@ class PoolsFragment : BaseFragment() {
 
         binding.viewModel = _viewModel
 
-        poolListenerRegistration = FirestoreUtil.addPoolsListener(requireActivity(), this::updateRecyclerView)
+        poolListenerRegistration =
+            FirestoreUtil.addPoolsListener(requireActivity(), this::updateRecyclerView)
 
-        binding.fab.setOnClickListener {
+        // TODO: Move to ViewModel
+        binding.newPoolFab.setOnClickListener {
             _viewModel.navigationCommand.postValue(NavigationCommand.To(PoolsFragmentDirections.actionNavPoolsToAddPoolFragment()))
+        }
+        // TODO: Move to ViewModel
+        binding.expandFab.setOnClickListener {
+            onExpandButtonClicked()
         }
 
         return binding.root
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
         FirestoreUtil.removeListener(poolListenerRegistration)
         shouldInitRecyclerView = true
         _binding = null
+    }
+
+    // TODO: Move to ViewModel
+    private fun onExpandButtonClicked() {
+        setAnimation(fabClicked)
+        setFabVisibility(fabClicked)
+        setClickable(fabClicked)
+        fabClicked = !fabClicked
+    }
+
+    // TODO: Move to ViewModel
+    private fun setFabVisibility(clicked: Boolean) {
+        if (!clicked) {
+            binding.newPoolFab.visibility = View.VISIBLE
+            binding.joinPoolFab.visibility = View.VISIBLE
+        } else {
+            binding.newPoolFab.visibility = View.GONE
+            binding.joinPoolFab.visibility = View.GONE
+        }
+    }
+
+    // TODO: Move to ViewModel
+    private fun setAnimation(clicked: Boolean) {
+        if (!clicked) {
+            binding.newPoolFab.startAnimation(fromBottom)
+            binding.joinPoolFab.startAnimation(fromBottom)
+            binding.expandFab.startAnimation(rotateOpen)
+        } else {
+            binding.newPoolFab.startAnimation(toBottom)
+            binding.joinPoolFab.startAnimation(toBottom)
+            binding.expandFab.startAnimation(rotateClose)
+        }
+    }
+
+    // TODO: Move to ViewModel
+    private fun setClickable(clicked: Boolean) {
+        if (!clicked) {
+            binding.newPoolFab.isClickable = true
+            binding.joinPoolFab.isClickable = true
+        } else {
+            binding.newPoolFab.isClickable = false
+            binding.joinPoolFab.isClickable = false
+        }
     }
 
     private fun updateRecyclerView(items: List<BindableItem<PoolListItemBinding>>) {
@@ -75,17 +154,17 @@ class PoolsFragment : BaseFragment() {
                     poolSection = Section(items)
                     add(poolSection)
                 }
-                
+
             }
             shouldInitRecyclerView = false
         }
+
         fun updateItems() {
 
         }
-        if (shouldInitRecyclerView){
+        if (shouldInitRecyclerView) {
             init()
-        }
-        else {
+        } else {
             updateItems()
         }
     }
