62df462d47c7f7002a2492e83f052f6890a1f20f
Grayson Ruffo <93240608+g-ruffo@users.noreply.github.com>
2022-10-16 22:16:57 -0700

    feat: Set header text and image to current user profile

--- app/src/main/java/ca/veltus/wraproulette/ui/WrapRouletteActivity.kt
+++ app/src/main/java/ca/veltus/wraproulette/ui/WrapRouletteActivity.kt
@@ -1,20 +1,26 @@
 package ca.veltus.wraproulette.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.viewModels
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
 import ca.veltus.wraproulette.R
+import ca.veltus.wraproulette.authentication.LoginSignupActivity
 import ca.veltus.wraproulette.authentication.LoginSignupViewModel
 import ca.veltus.wraproulette.databinding.ActivityWrapRouletteBinding
+import ca.veltus.wraproulette.utils.FirebaseStorageUtil
+import ca.veltus.wraproulette.utils.FirestoreUtil
+import com.bumptech.glide.Glide
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.AndroidEntryPoint
@@ -72,8 +78,26 @@ class WrapRouletteActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
+        // Get Firebase current user data and display it in header.
+        FirestoreUtil.getCurrentUser { user ->
+            navView.getHeaderView(0).findViewById<TextView>(R.id.userNameHeaderTextView).text = user.displayName
+            navView.getHeaderView(0).findViewById<TextView>(R.id.userEmailHeaderTextView).text = user.email
+
+                if (user.profilePicturePath != null) {
+                    Glide.with(this).load(FirebaseStorageUtil.pathToReference(user.profilePicturePath))
+                        .placeholder(R.drawable.ic_baseline_account_circle_24)
+                        .into(navView.getHeaderView(0).findViewById<ImageView>(R.id.profileHeaderImageView))
+                }
+            }
+
+
         navView.menu.findItem(R.id.logoutMenu).setOnMenuItemClickListener {
             viewModel.logout()
+            startActivity(
+                Intent(this, LoginSignupActivity::class.java)
+                    .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            )
+            finish()
             true
         }
     }
