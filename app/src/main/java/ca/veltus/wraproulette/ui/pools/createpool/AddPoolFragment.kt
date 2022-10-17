4447ee9cdffaddc19ca2b954fe22812c416dc201
Grayson Ruffo <93240608+g-ruffo@users.noreply.github.com>
2022-10-16 22:19:32 -0700

    feat: Add date picker to fragment_add_pool.xml

--- app/src/main/java/ca/veltus/wraproulette/ui/pools/createpool/AddPoolFragment.kt
+++ app/src/main/java/ca/veltus/wraproulette/ui/pools/createpool/AddPoolFragment.kt
@@ -1,7 +1,7 @@
 package ca.veltus.wraproulette.ui.pools.createpool
 
+import android.app.DatePickerDialog
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,6 +15,8 @@ import ca.veltus.wraproulette.utils.FirestoreUtil
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import dagger.hilt.android.AndroidEntryPoint
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 @AndroidEntryPoint
@@ -28,6 +30,7 @@ class AddPoolFragment : BaseFragment() {
     private lateinit var databaseReference: FirebaseFirestore
 
     private var _binding: FragmentAddPoolBinding? = null
+
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
@@ -44,8 +47,11 @@ class AddPoolFragment : BaseFragment() {
         binding.viewModel = _viewModel
 
         binding.createButton.setOnClickListener {
-            Log.i(TAG, "onCreateView: ${_viewModel.poolProduction.value!!}")
-            FirestoreUtil.createPool(_viewModel.poolProduction.value!!, _viewModel.poolPassword.value!!) {
+            FirestoreUtil.createPool(
+                _viewModel.poolProduction.value!!,
+                _viewModel.poolPassword.value!!,
+                _viewModel.poolDate.value!!
+            ) {
                 _viewModel.navigateBack()
             }
         }
@@ -53,9 +59,56 @@ class AddPoolFragment : BaseFragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        binding.selectDateAutoComplete.setOnFocusChangeListener { v, hasFocus ->
+            if (hasFocus) {
+                v.performClick()
+            }
+        }
+
+        binding.selectDateAutoComplete.setOnClickListener {
+            launchDatePickerDialog()
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    // Launch date dialog and listen for its result.
+    private fun launchDatePickerDialog() {
+        val calendar = Calendar.getInstance()
+        var selectedYear = calendar.get(Calendar.YEAR)
+        var selectedMonth = calendar.get(Calendar.MONTH)
+        var selectedDay = calendar.get(Calendar.DAY_OF_MONTH)
+
+        val datePickerListener =
+            DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+                calendar.set(Calendar.YEAR, year)
+                calendar.set(Calendar.MONTH, month)
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+//                _viewModel.setPoolDate(calendar.time)
+                _viewModel.setPoolDate(
+                    SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH).format(
+                        calendar.time
+                    )
+                )
+            }
+
+        val dialog = DatePickerDialog(
+            requireContext(),
+            datePickerListener,
+            selectedYear,
+            selectedMonth,
+            selectedDay
+        )
+
+        dialog.show()
     }
 
 }
