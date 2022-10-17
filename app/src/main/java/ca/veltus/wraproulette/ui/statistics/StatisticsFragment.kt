cb5f3a52b3dfaa348260fc872cd4eb1903bf9fd0
Grayson Ruffo <93240608+g-ruffo@users.noreply.github.com>
2022-10-16 19:20:29 -0700

    chore: Initial setup for StatisticsFragment.kt

--- app/src/main/java/ca/veltus/wraproulette/ui/statistics/StatisticsFragment.kt
+++ app/src/main/java/ca/veltus/wraproulette/ui/statistics/StatisticsFragment.kt
@@ -5,17 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import ca.veltus.wraproulette.base.BaseFragment
 import ca.veltus.wraproulette.databinding.FragmentStatisticsBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-class StatisticsFragment : Fragment() {
+@AndroidEntryPoint
+class StatisticsFragment : BaseFragment() {
 
     private var _binding: FragmentStatisticsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    override val _viewModel by viewModels<StatisticsViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
