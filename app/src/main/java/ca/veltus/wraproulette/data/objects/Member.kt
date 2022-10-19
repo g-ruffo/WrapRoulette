e48a3dfe6bad0e2a82a189b07de1cdb25e1660a6
Grayson Ruffo <93240608+g-ruffo@users.noreply.github.com>
2022-10-18 20:23:15 -0700

    feat: Create Member.kt class and member list item

---
+++ app/src/main/java/ca/veltus/wraproulette/data/objects/Member.kt
@@ -0,0 +1,33 @@
+package ca.veltus.wraproulette.data.objects
+
+import ca.veltus.wraproulette.R
+import ca.veltus.wraproulette.databinding.MemberListItemBinding
+import com.xwray.groupie.databinding.BindableItem
+
+data class Member(
+    val uid: String,
+    val poolId: String,
+    val displayName: String,
+    val email: String,
+    val department: String?,
+    val bidTime: String?,
+    val profilePicturePath: String?
+) {
+    constructor() : this("", "", "", "", null, null, null)
+}
+
+class MemberItem(val member: Member
+) : BindableItem<MemberListItemBinding>() {
+
+    override fun bind(viewBinding: MemberListItemBinding, position: Int) {
+        viewBinding.member = member
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.member_list_item
+    }
+
+    companion object {
+        private const val TAG = "Member"
+    }
+}
