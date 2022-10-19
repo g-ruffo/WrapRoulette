06df79bcd654fc85994364ddaffb5837aa77bdcc
Grayson Ruffo <93240608+g-ruffo@users.noreply.github.com>
2022-10-18 20:29:56 -0700

    refactor: Remove ViewPagerAdapter.kt imports

--- app/src/main/java/ca/veltus/wraproulette/ui/home/ViewPagerAdapter.kt
+++ app/src/main/java/ca/veltus/wraproulette/ui/home/ViewPagerAdapter.kt
@@ -1,8 +1,6 @@
 package ca.veltus.wraproulette.ui.home
 
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import ca.veltus.wraproulette.ui.home.bids.BidsFragment
 import ca.veltus.wraproulette.ui.home.chat.ChatFragment
