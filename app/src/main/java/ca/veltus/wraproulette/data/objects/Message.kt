0f20c9557e4c5646fcccd125938a57ff90e6e524
Grayson Ruffo <93240608+g-ruffo@users.noreply.github.com>
2022-10-19 22:20:14 -0700

    feat: Create Groupie data item for both to and from messages

--- app/src/main/java/ca/veltus/wraproulette/data/objects/Message.kt
+++ app/src/main/java/ca/veltus/wraproulette/data/objects/Message.kt
@@ -1,27 +1,39 @@
 package ca.veltus.wraproulette.data.objects
 
 import ca.veltus.wraproulette.R
-import ca.veltus.wraproulette.databinding.MessageListItemBinding
+import ca.veltus.wraproulette.databinding.MessageListItemFromUserBinding
+import ca.veltus.wraproulette.databinding.MessageListItemToUserBinding
 import com.xwray.groupie.databinding.BindableItem
-import java.sql.Date
+import java.util.*
 
 data class Message(
     val text: String,
     val time: Date,
-    val senderUid: String
+    val senderUid: String,
+    val profilePicture: String?,
+    val messageUid: String
 ) {
-    constructor() : this("", Date(0), "")
+    constructor() : this("", Date(0), "", null, "")
 }
 
-class MessageItem(val message: Message) : BindableItem<MessageListItemBinding>() {
+class MessageItemFrom(val message: Message) : BindableItem<MessageListItemFromUserBinding>() {
 
-    override fun bind(viewBinding: MessageListItemBinding, position: Int) {
+    override fun bind(viewBinding: MessageListItemFromUserBinding, position: Int) {
         viewBinding.message = message
     }
+
     override fun getLayout(): Int {
-        return R.layout.message_list_item
+        return R.layout.message_list_item_from_user
+    }
+}
+
+class MessageItemTo(val message: Message) : BindableItem<MessageListItemToUserBinding>() {
+
+    override fun bind(viewBinding: MessageListItemToUserBinding, position: Int) {
+        viewBinding.message = message
     }
 
-
-
+    override fun getLayout(): Int {
+        return R.layout.message_list_item_to_user
+    }
 }
