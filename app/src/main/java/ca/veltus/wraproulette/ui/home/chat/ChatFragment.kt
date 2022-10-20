374207382e921ee6574212bf8a5f586220d90c53
Grayson Ruffo <93240608+g-ruffo@users.noreply.github.com>
2022-10-20 16:18:19 -0700

    feat: Setup Groupie RecyclerView with ChatFragment.kt

--- app/src/main/java/ca/veltus/wraproulette/ui/home/chat/ChatFragment.kt
+++ app/src/main/java/ca/veltus/wraproulette/ui/home/chat/ChatFragment.kt
@@ -1,14 +1,25 @@
 package ca.veltus.wraproulette.ui.home.chat
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import ca.veltus.wraproulette.base.BaseFragment
+import ca.veltus.wraproulette.data.objects.Message
+import ca.veltus.wraproulette.data.objects.MessageItemFrom
+import ca.veltus.wraproulette.data.objects.MessageItemTo
 import ca.veltus.wraproulette.databinding.FragmentChatBinding
 import ca.veltus.wraproulette.ui.home.HomeViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.xwray.groupie.GroupieAdapter
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class ChatFragment : BaseFragment() {
@@ -16,21 +27,18 @@ class ChatFragment : BaseFragment() {
         private const val TAG = "ChatFragment"
     }
 
-    private var _binding: FragmentChatBinding? = null
+    private lateinit var binding: FragmentChatBinding
     override val _viewModel by viewModels<HomeViewModel>()
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentChatBinding.inflate(inflater, container, false)
+        binding = FragmentChatBinding.inflate(inflater, container, false)
 
         binding.viewModel = _viewModel
+
+        _viewModel.hideFab()
 
         return binding.root
     }
@@ -38,6 +46,44 @@ class ChatFragment : BaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.lifecycleOwner = viewLifecycleOwner
+        Log.i(TAG, "onViewCreated: Called")
+        lifecycleScope.launch {
+            Log.i(TAG, "lifecycleScope: Called")
+            _viewModel._chatList.collect {
+                Log.i(TAG, "onViewCreated: $it")
+                setupRecyclerView(it)
+            }
+        }
+
+        binding.sendMessageButton.setOnClickListener {
+            _viewModel.sendChatMessage {
+                val inputMethodManager =
+                    requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                inputMethodManager.hideSoftInputFromWindow(requireView().windowToken, 0)
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        _viewModel.getChatData()
+    }
+
+    private fun setupRecyclerView(items: List<Message>) {
+        val groupieAdapter = GroupieAdapter()
+        items.forEach {
+            if (it.senderUid == FirebaseAuth.getInstance().currentUser!!.uid) {
+                groupieAdapter.add(MessageItemFrom(it))
+            } else {
+                groupieAdapter.add(MessageItemTo(it))
+            }
+        }
+
+        binding.chatRecyclerView.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = groupieAdapter
+            scrollToPosition(items.size - 1)
+        }
     }
 
 }
