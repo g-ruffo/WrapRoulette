24826bd9f75c771710c152d6aaf65ec37f385e12
Grayson Ruffo <93240608+g-ruffo@users.noreply.github.com>
2022-10-20 18:27:02 -0700

    feat: Create lifecycle aware extension function for listener

--- app/src/main/java/ca/veltus/wraproulette/utils/Extensions.kt
+++ app/src/main/java/ca/veltus/wraproulette/utils/Extensions.kt
@@ -1,6 +1,11 @@
 package ca.veltus.wraproulette.utils
 
-import ca.veltus.wraproulette.data.objects.*
+import androidx.lifecycle.LifecycleOwner
+import androidx.viewpager2.widget.ViewPager2
+import ca.veltus.wraproulette.data.objects.Member
+import ca.veltus.wraproulette.data.objects.MemberItem
+import ca.veltus.wraproulette.data.objects.Pool
+import ca.veltus.wraproulette.data.objects.PoolItem
 import com.google.android.gms.tasks.Task
 import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlin.coroutines.resumeWithException
@@ -18,14 +23,28 @@ suspend fun <T> Task<T>.await(): T {
     }
 }
 
-fun List<Member>.toMemberItem() : List<MemberItem> {
+fun List<Member>.toMemberItem(): List<MemberItem> {
     return this.map {
         MemberItem(it)
     }
 }
 
-fun List<Pool>.toPoolItem() : List<PoolItem> {
+fun List<Pool>.toPoolItem(): List<PoolItem> {
     return this.map {
         PoolItem(it)
+    }
+}
+
+
+inline fun ViewPager2.onPageSelected(
+    lifecycleOwner: LifecycleOwner,
+    crossinline listener: (position: Int) -> Unit
+) {
+    object : ViewPager2.OnPageChangeCallback() {
+        override fun onPageSelected(position: Int) = listener(position)
+    }.let {
+        LifecycleEventDispatcher(lifecycleOwner,
+            onStart = { registerOnPageChangeCallback(it) },
+            onStop = { unregisterOnPageChangeCallback(it) })
     }
 }
