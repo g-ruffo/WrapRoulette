911d277f2fe1803a8c66cf8b7e8c782753c3564d
Grayson Ruffo <93240608+g-ruffo@users.noreply.github.com>
2022-10-21 16:14:41 -0700

    feat: Set user bet date to the active pool date

--- app/src/main/java/ca/veltus/wraproulette/ui/home/dialog/BetDialogFragment.kt
+++ app/src/main/java/ca/veltus/wraproulette/ui/home/dialog/BetDialogFragment.kt
@@ -8,21 +8,20 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.DialogFragment
-import androidx.fragment.app.viewModels
 import ca.veltus.wraproulette.databinding.FragmentBetDialogBinding
-import ca.veltus.wraproulette.ui.home.HomeViewModel
 import ca.veltus.wraproulette.utils.FirestoreUtil
 import dagger.hilt.android.AndroidEntryPoint
 import java.util.*
 
 @AndroidEntryPoint
-class BetDialogFragment : DialogFragment() {
+class BetDialogFragment(poolDate: Date?) : DialogFragment() {
     companion object {
         private const val TAG = "BetDialogFragment"
     }
 
     private var _binding: FragmentBetDialogBinding? = null
-//    val viewMode by viewModels<HomeViewModel>()
+
+    private val date = poolDate
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -35,25 +34,31 @@ class BetDialogFragment : DialogFragment() {
     ): View {
         _binding = FragmentBetDialogBinding.inflate(inflater, container, false)
 
+        binding.timePickerSpinner.setIs24HourView(true)
+        binding.timePickerSpinner.hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
+        binding.timePickerSpinner.minute = Calendar.getInstance().get(Calendar.MINUTE)
+
         binding.cancelBetButton.setOnClickListener {
             dismiss()
         }
         binding.placeBetButton.setOnClickListener {
             val calendar = Calendar.getInstance()
-
             calendar.set(Calendar.HOUR_OF_DAY, binding.timePickerSpinner.hour)
             calendar.set(Calendar.MINUTE, binding.timePickerSpinner.minute)
+            calendar.set(Calendar.DATE, date!!.date)
+            calendar.set(Calendar.DATE, date!!.month)
+            calendar.set(Calendar.DATE, date!!.year)
+
             FirestoreUtil.getCurrentUser { user ->
-                FirestoreUtil.setUserPoolBet(user.activePool!!, user.uid, Date(calendar.time.time)) {
+                FirestoreUtil.setUserPoolBet(
+                    user.activePool!!,
+                    user.uid,
+                    Date(calendar.time.time)
+                ) {
                     dismiss()
                 }
             }
         }
-
-        binding.timePickerSpinner.setIs24HourView(true)
-        binding.timePickerSpinner.hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
-        binding.timePickerSpinner.minute = Calendar.getInstance().get(Calendar.MINUTE)
-
 
         return binding.root
     }
@@ -66,7 +71,10 @@ class BetDialogFragment : DialogFragment() {
 
     override fun onStart() {
         super.onStart()
-        dialog!!.window!!.setLayout(ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.WRAP_CONTENT)
+        dialog!!.window!!.setLayout(
+            ConstraintLayout.LayoutParams.MATCH_PARENT,
+            ConstraintLayout.LayoutParams.WRAP_CONTENT
+        )
         dialog!!.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
 
     }
