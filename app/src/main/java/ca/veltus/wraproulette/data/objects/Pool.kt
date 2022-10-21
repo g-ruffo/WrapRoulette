c2f915e095ca88fbef81f06b86e6288518fde9be
Grayson Ruffo <93240608+g-ruffo@users.noreply.github.com>
2022-10-20 19:34:37 -0700

    feat: Add navigation command for PoolItem onClickListener

--- app/src/main/java/ca/veltus/wraproulette/data/objects/Pool.kt
+++ app/src/main/java/ca/veltus/wraproulette/data/objects/Pool.kt
@@ -1,8 +1,15 @@
 package ca.veltus.wraproulette.data.objects
 
+import android.graphics.Color
+import android.graphics.drawable.Drawable
 import android.view.View
+import androidx.annotation.DrawableRes
+import androidx.core.content.ContextCompat
+import androidx.navigation.Navigation
 import ca.veltus.wraproulette.R
+import ca.veltus.wraproulette.base.NavigationCommand
 import ca.veltus.wraproulette.databinding.PoolListItemBinding
+import ca.veltus.wraproulette.ui.pools.PoolsFragmentDirections
 import ca.veltus.wraproulette.utils.FirestoreUtil
 import com.google.firebase.Timestamp
 import com.xwray.groupie.databinding.BindableItem
@@ -20,27 +27,38 @@ data class Pool(
     var endTime: Timestamp?,
     val users: MutableMap<String, Any>? = null
 ) {
-    constructor(): this("", "", "", "", "", null, null, null, null, null, mutableMapOf())
+    constructor() : this("", "", "", "", "", null, null, null, null, null, mutableMapOf())
 
 }
 
-class PoolItem(val pool: Pool
+class PoolItem(
+    val pool: Pool
 ) : BindableItem<PoolListItemBinding>() {
 
     override fun bind(viewBinding: PoolListItemBinding, position: Int) {
         viewBinding.pool = pool
         FirestoreUtil.getCurrentUser { user ->
             if (pool.docId == user.activePool) {
-                viewBinding.selectedPoolIcon.visibility = View.VISIBLE
-
+                viewBinding.cardView.setBackgroundColor(
+                    ContextCompat.getColor(
+                        viewBinding.root.context,
+                        R.color.selectedPoolCardView
+                    )
+                )
             } else {
-                viewBinding.selectedPoolIcon.visibility = View.GONE
+                viewBinding.cardView.setBackgroundColor(Color.WHITE)
             }
             user.activePool
         }
         viewBinding.cardView.setOnClickListener {
             FirestoreUtil.setActivePool(pool.docId) {
-                viewBinding.selectedPoolIcon.visibility = View.VISIBLE
+                viewBinding.cardView.setBackgroundColor(
+                    ContextCompat.getColor(
+                        viewBinding.root.context,
+                        R.color.selectedPoolCardView
+                    )
+                )
+                Navigation.findNavController(viewBinding.root).navigate(PoolsFragmentDirections.actionNavPoolsToNavHome())
             }
         }
     }
