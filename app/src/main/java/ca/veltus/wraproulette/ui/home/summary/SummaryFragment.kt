3d9d23b6e36e2eb33e7b5980c700384340b72b21
Grayson Ruffo <93240608+g-ruffo@users.noreply.github.com>
2022-10-26 00:05:28 -0700

    feat: Add a recyclerView of active bids to SummaryFragment.kt

--- app/src/main/java/ca/veltus/wraproulette/ui/home/summary/SummaryFragment.kt
+++ app/src/main/java/ca/veltus/wraproulette/ui/home/summary/SummaryFragment.kt
@@ -1,14 +1,23 @@
 package ca.veltus.wraproulette.ui.home.summary
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.LinearLayoutManager
 import ca.veltus.wraproulette.base.BaseFragment
+import ca.veltus.wraproulette.data.objects.MemberItem
 import ca.veltus.wraproulette.databinding.FragmentSummaryBinding
 import ca.veltus.wraproulette.ui.home.HomeViewModel
+import ca.veltus.wraproulette.utils.toMemberItem
+import com.xwray.groupie.GroupieAdapter
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class SummaryFragment : BaseFragment() {
@@ -38,5 +47,25 @@ class SummaryFragment : BaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.lifecycleOwner = viewLifecycleOwner
+
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                _viewModel.poolTotalBets.collect {
+                    Log.i(TAG, "onViewCreated: ${it}")
+                    setupRecyclerView(it.toMemberItem())
+                }
+            }
+        }
+    }
+
+
+    private fun setupRecyclerView(items: List<MemberItem>) {
+        val groupieAdapter = GroupieAdapter().apply {
+            addAll(items.sortedBy { it.member.bidTime })
+        }
+        binding.memberBidsRecyclerView.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = groupieAdapter
+        }
     }
 }
