2e011fca4763be0a1f335d45043cad8423e0abec
Grayson Ruffo <93240608+g-ruffo@users.noreply.github.com>
2022-10-25 20:04:56 -0700

    refactor: Create second module to store Firebase instances

--- app/src/main/java/ca/veltus/wraproulette/di/AppModule.kt
+++ app/src/main/java/ca/veltus/wraproulette/di/AppModule.kt
@@ -3,19 +3,24 @@ package ca.veltus.wraproulette.di
 import ca.veltus.wraproulette.data.repository.AuthenticationRepository
 import ca.veltus.wraproulette.data.repository.AuthenticationRepositoryImpl
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 class AppModule {
 
     @Provides
-    fun provideFirebaseAuth(): FirebaseAuth = FirebaseAuth.getInstance()
-
-    @Provides
-    fun provideAuthenticationRepository(implementation: AuthenticationRepositoryImpl): AuthenticationRepository = implementation
+    @Singleton
+    fun provideAuthenticationRepository(
+        firebaseAuth: FirebaseAuth,
+        firestore: FirebaseFirestore
+    ): AuthenticationRepository {
+        return AuthenticationRepositoryImpl(firebaseAuth, firestore)
+    }
 
 }
---
+++ app/src/main/java/ca/veltus/wraproulette/di/FirebaseModule.kt
@@ -0,0 +1,23 @@
+package ca.veltus.wraproulette.di
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+class FirebaseModule {
+
+    @Provides
+    @Singleton
+    fun provideFirebaseAuth(): FirebaseAuth = FirebaseAuth.getInstance()
+
+    @Provides
+    @Singleton
+    fun provideFirestoreInstance(): FirebaseFirestore = FirebaseFirestore.getInstance()
+
+}
