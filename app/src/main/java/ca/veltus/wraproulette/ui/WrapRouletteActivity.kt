4f1d6b4a482ad99d90aebb7d6396551d88ab4107
Grayson Ruffo <93240608+g-ruffo@users.noreply.github.com>
2022-10-26 23:36:25 -0700

    feat: Connect toolbar and toggle in WrapRouletteActivity.kt

--- app/src/main/java/ca/veltus/wraproulette/ui/WrapRouletteActivity.kt
+++ app/src/main/java/ca/veltus/wraproulette/ui/WrapRouletteActivity.kt
@@ -2,10 +2,10 @@ package ca.veltus.wraproulette.ui
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.viewModels
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
@@ -21,8 +21,6 @@ import ca.veltus.wraproulette.utils.FirebaseStorageUtil
 import ca.veltus.wraproulette.utils.FirestoreUtil
 import com.bumptech.glide.Glide
 import com.google.android.material.navigation.NavigationView
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -35,28 +33,18 @@ class WrapRouletteActivity : AppCompatActivity() {
     private val viewModel: LoginSignupViewModel by viewModels()
 
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var drawerToggle: ActionBarDrawerToggle
     private lateinit var binding: ActivityWrapRouletteBinding
-
-    val db = Firebase.firestore
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityWrapRouletteBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         setSupportActionBar(binding.appBarMain.toolbar)
 
-
         setupNavigationAndToolbar()
 
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -68,6 +56,16 @@ class WrapRouletteActivity : AppCompatActivity() {
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
+
+        drawerToggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            binding.appBarMain.toolbar,
+            R.string.open,
+            R.string.close
+        )
+        drawerLayout.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
