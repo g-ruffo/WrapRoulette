03905e03323643cb0f35175a23bdf2224125baf7
Grayson Ruffo <93240608+g-ruffo@users.noreply.github.com>
2022-11-16 18:22:28 -0800

    style: Create PIR vector images and selector

--- app/src/main/java/ca/veltus/wraproulette/utils/Constants.kt
+++ app/src/main/java/ca/veltus/wraproulette/utils/Constants.kt
@@ -5,5 +5,9 @@ object Constants {
     const val MINUTE = 60000
     const val HOUR = 3600000
     const val DAY = 86400000
+    const val BID_DIALOG = 0
+    const val ADMIN_DIALOG = 1
+    const val MARGIN_DIALOG = 2
+    const val PIR_DIALOG = 3
 
 }
