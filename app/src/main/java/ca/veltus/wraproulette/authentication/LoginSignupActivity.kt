5d84d75e72d02c4efc7f4a3df460ccf9e34b8e94
Grayson Ruffo <93240608+g-ruffo@users.noreply.github.com>
2022-11-27 22:48:19 -0800

    feat: Add splash screen api

--- app/src/main/java/ca/veltus/wraproulette/authentication/LoginSignupActivity.kt
+++ app/src/main/java/ca/veltus/wraproulette/authentication/LoginSignupActivity.kt
@@ -1,11 +1,14 @@
 package ca.veltus.wraproulette.authentication
 
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
 import android.view.inputmethod.InputMethodManager
 import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import ca.veltus.wraproulette.databinding.ActivityLoginSignupBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -20,15 +23,16 @@ class LoginSignupActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginSignupBinding
 
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         binding = ActivityLoginSignupBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         //Set activity as fullscreen by hiding the status bar, bottom navigation bar and action bar
         this.window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
+            WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
         supportActionBar?.hide()
