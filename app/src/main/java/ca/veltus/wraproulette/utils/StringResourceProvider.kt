b41f662cef7275d17c55ef7cf5cb4f862e2afb34
Grayson Ruffo <93240608+g-ruffo@users.noreply.github.com>
2022-12-05 02:37:26 -0800

    feat: Create StringResourcesProvider class
    Used to retrieve string resources in the viewModels.

---
+++ app/src/main/java/ca/veltus/wraproulette/utils/StringResourceProvider.kt
@@ -0,0 +1,14 @@
+package ca.veltus.wraproulette.utils
+
+import android.content.Context
+import androidx.annotation.StringRes
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class StringResourcesProvider @Inject constructor(@ApplicationContext private val context: Context) {
+    fun getString(@StringRes stringResId: Int): String {
+        return context.getString(stringResId)
+    }
+}
