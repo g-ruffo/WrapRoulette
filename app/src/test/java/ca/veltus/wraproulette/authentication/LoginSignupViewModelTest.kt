339b9c3111b596520222800e4e25c9f23aa56535
Grayson Ruffo <93240608+g-ruffo@users.noreply.github.com>
2022-12-17 18:51:10 -0800

    test: Create tests for view model

--- app/src/test/java/ca/veltus/wraproulette/authentication/LoginSignupViewModelTest.kt
+++ app/src/test/java/ca/veltus/wraproulette/authentication/LoginSignupViewModelTest.kt
@@ -46,21 +46,117 @@ class LoginSignupViewModelTest {
 
     @Test
     fun `resetPassword function, returns error when email is blank`() = runBlocking {
-        viewModel.emailAddress.postValue("  ")
+        viewModel.emailAddress.value = "  "
 
         viewModel.resetPassword()
 
         assertThat(viewModel.errorEmailText.value).isEqualTo(ErrorMessage.ErrorText("Please enter your email address."))
+        assertThat(viewModel.showLoading.value).isEqualTo(false)
+    }
+
+    @Test
+    fun `resetPassword function, returns error when email is not proper format`() = runBlocking {
+        viewModel.emailAddress.value = "John.Doe@google"
+
+        viewModel.resetPassword()
+
+        assertThat(viewModel.errorEmailText.value).isEqualTo(ErrorMessage.ErrorText("Email address is not valid."))
+        assertThat(viewModel.showLoading.value).isEqualTo(false)
+
     }
 
 
     @Test
     fun `validateEmailAndPassword function, returns error when email is blank`() {
-        viewModel.emailAddress.postValue("  ")
+        viewModel.emailAddress.value = "  "
 
         val result = viewModel.validateEmailAndPassword()
 
         assertThat(viewModel.errorEmailText.value).isEqualTo(ErrorMessage.ErrorText("Please enter your email address."))
         assertThat(result).isEqualTo(false)
+        assertThat(viewModel.showLoading.value).isEqualTo(false)
     }
+
+    @Test
+    fun `validateEmailAndPassword function, returns error when email is not proper format`() {
+        viewModel.emailAddress.value = "John.Doe@google     "
+
+        val result = viewModel.validateEmailAndPassword()
+
+        assertThat(viewModel.errorEmailText.value).isEqualTo(ErrorMessage.ErrorText("Email address is not valid."))
+        assertThat(viewModel.emailAddress.value).isEqualTo("John.Doe@google")
+        assertThat(result).isEqualTo(false)
+        assertThat(viewModel.showLoading.value).isEqualTo(false)
+    }
+
+    @Test
+    fun `validateEmailAndPassword function, returns error when password is blank`() {
+        viewModel.password.value = "  "
+        viewModel.emailAddress.value = "John.Doe@google.com"
+
+        val result = viewModel.validateEmailAndPassword()
+
+        assertThat(viewModel.errorPasswordText.value).isEqualTo(ErrorMessage.ErrorText("Please enter a password."))
+        assertThat(result).isEqualTo(false)
+        assertThat(viewModel.showLoading.value).isEqualTo(false)
+    }
+
+    @Test
+    fun `validateEmailAndPassword function, returns error when username is blank`() {
+        viewModel.username.value = "  "
+        viewModel.password.value = "password"
+        viewModel.emailAddress.value = "John.Doe@google.com"
+
+        val result = viewModel.validateEmailAndPassword(true)
+
+        assertThat(viewModel.errorNameText.value).isEqualTo(ErrorMessage.ErrorText("Please enter your name."))
+        assertThat(result).isEqualTo(false)
+        assertThat(viewModel.showLoading.value).isEqualTo(false)
+    }
+
+    @Test
+    fun `validateEmailAndPassword function, returns error when department is blank`() {
+        viewModel.username.value = "John"
+        viewModel.password.value = "password"
+        viewModel.emailAddress.value = "John.Doe@google.com"
+        viewModel.department.value = "   "
+
+        val result = viewModel.validateEmailAndPassword(true)
+
+        assertThat(viewModel.errorDepartmentText.value).isEqualTo(ErrorMessage.ErrorText("Please enter your department."))
+        assertThat(result).isEqualTo(false)
+        assertThat(viewModel.showLoading.value).isEqualTo(false)
+    }
+
+    @Test
+    fun `validateUpdateCurrentUser function, returns error when username is blank`() {
+        viewModel.updateDepartment.value = ""
+
+        viewModel.validateUpdateCurrentUser()
+
+        assertThat(viewModel.errorNameText.value).isEqualTo(ErrorMessage.ErrorText("Please enter your name."))
+        assertThat(viewModel.showLoading.value).isEqualTo(false)
+    }
+
+    @Test
+    fun `validateUpdateCurrentUser function, returns error when department is blank`() {
+        viewModel.updateUsername.value = "John"
+        viewModel.updateDepartment.value = ""
+
+        viewModel.validateUpdateCurrentUser()
+
+        assertThat(viewModel.errorDepartmentText.value).isEqualTo(ErrorMessage.ErrorText("Please enter your department."))
+        assertThat(viewModel.showLoading.value).isEqualTo(false)
+    }
+
+    @Test
+    fun `sendFeedbackMessage function, returns false when message is blank`() {
+        viewModel.feedbackMessage.value = "    "
+
+        val result = viewModel.sendFeedbackMessage()
+
+        assertThat(result).isEqualTo(false)
+        assertThat(viewModel.showLoading.value).isEqualTo(false)
+    }
+
 }
