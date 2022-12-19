f264c7e0f17e1832d2c7cbfbf9f118a963aa01b1
Grayson Ruffo <93240608+g-ruffo@users.noreply.github.com>
2022-12-19 00:39:46 -0800

    feat: Implement photo picker api

--- app/src/main/java/ca/veltus/wraproulette/ui/account/AccountFragment.kt
+++ app/src/main/java/ca/veltus/wraproulette/ui/account/AccountFragment.kt
@@ -1,18 +1,18 @@
 package ca.veltus.wraproulette.ui.account
 
-import android.Manifest.permission.READ_EXTERNAL_STORAGE
-import android.app.Activity
-import android.content.Intent
-import android.content.pm.PackageManager
 import android.database.Cursor
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.app.ActivityCompat
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
@@ -33,10 +33,6 @@ import java.io.ByteArrayOutputStream
 
 @AndroidEntryPoint
 class AccountFragment : BaseFragment() {
-    companion object {
-        private const val RC_SELECT_IMAGE = 2
-        private const val STORAGE_REQUEST_CODE = 8
-    }
 
     override val _viewModel by viewModels<LoginSignupViewModel>()
 
@@ -46,6 +42,34 @@ class AccountFragment : BaseFragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    // Registers a photo picker activity launcher in single-select mode.
+    private val photoPicker =
+        registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+            // Callback is invoked after the user selects a media item or closes the photo picker.
+            if (uri != null) {
+                val bitmap = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                    ImageDecoder.decodeBitmap(
+                        ImageDecoder.createSource(
+                            requireActivity().contentResolver, uri
+                        )
+                    )
+                } else {
+                    @Suppress("DEPRECATION") rotateBitmap(
+                        getPathFromUri(uri),
+                        MediaStore.Images.Media.getBitmap(requireActivity().contentResolver, uri)
+                    )
+                }
+
+                val outputStream = ByteArrayOutputStream()
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 50, outputStream)
+                selectedImageBytes = outputStream.toByteArray()
+                _viewModel.setTemporaryProfileImage(selectedImageBytes)
+
+            } else {
+                Log.d("PhotoPicker", "No media selected")
+            }
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -93,76 +117,29 @@ class AccountFragment : BaseFragment() {
         _binding = null
     }
 
-    @Deprecated("Deprecated in Java")
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        @Suppress("DEPRECATION") super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode == RC_SELECT_IMAGE && resultCode == Activity.RESULT_OK && data != null && data.data != null) {
-            val selectedImagePath = data.data
-            val filePathColumn = arrayOf(MediaStore.Images.Media.DATA)
-
-            val cursor: Cursor = requireActivity().contentResolver!!.query(
-                selectedImagePath!!, filePathColumn, null, null, null
-            )!!
-
-            cursor.moveToFirst()
-            val columnIndex: Int = cursor.getColumnIndex(filePathColumn[0])
-            val picturePath: String = cursor.getString(columnIndex)
-            cursor.close()
-
-            val bitmap = BitmapFactory.decodeFile(picturePath)
-            val rotatedBitmap = rotateBitmap(picturePath, bitmap)
-            val outputStream = ByteArrayOutputStream()
-            rotatedBitmap.compress(Bitmap.CompressFormat.JPEG, 50, outputStream)
-            selectedImageBytes = outputStream.toByteArray()
-            _viewModel.setTemporaryProfileImage(selectedImageBytes)
-        }
-    }
-
-    @Deprecated("Deprecated in Java")
-    override fun onRequestPermissionsResult(
-        requestCode: Int, permissions: Array<out String>, grantResults: IntArray
-    ) {
-        @Suppress("DEPRECATION") super.onRequestPermissionsResult(
-            requestCode,
-            permissions,
-            grantResults
-        )
-        if (requestCode == STORAGE_REQUEST_CODE && grantResults[0] == PackageManager.PERMISSION_DENIED) {
-            // Permission is denied
-            _viewModel.postSnackBarMessage(getString(R.string.imagePermissionsNeededErrorMessage))
-        }
-    }
-
     private fun setupOnClickListeners() {
         binding.profilePictureImageView.setOnClickListener {
-            if (arePermissionsGranted()) {
-                val intent = Intent().apply {
-                    type = "image/*"
-                    action = Intent.ACTION_PICK
-                    putExtra(
-                        Intent.EXTRA_MIME_TYPES, arrayOf("image/jpeg", "image/png", "image/gif")
-                    )
-                }
-                @Suppress("DEPRECATION") startActivityForResult(
-                    Intent.createChooser(intent, "Select Image"), RC_SELECT_IMAGE
-                )
-            } else {
-                requestPermission()
-            }
+            // Launch the photo picker and allow the user to choose only images.
+            photoPicker.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
         }
     }
 
-    private fun arePermissionsGranted() = ActivityCompat.checkSelfPermission(
-        requireActivity(), READ_EXTERNAL_STORAGE
-    ) == PackageManager.PERMISSION_GRANTED
-
-    private fun requestPermission() {
-        if (!arePermissionsGranted()) {
-            val permissions = arrayOf(
-                READ_EXTERNAL_STORAGE
-            )
-            @Suppress("DEPRECATION") requestPermissions(permissions, STORAGE_REQUEST_CODE)
+    @Suppress("SENSELESS_COMPARISON")
+    private fun getPathFromUri(uri: Uri?): String {
+        var result: String? = null
+        val projection = arrayOf(MediaStore.Images.Media.DATA)
+        val cursor: Cursor =
+            requireActivity().contentResolver.query(uri!!, projection, null, null, null)!!
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                val columnIndex = cursor.getColumnIndexOrThrow(projection[0])
+                result = cursor.getString(columnIndex)
+            }
+            cursor.close()
         }
+        if (result == null) {
+            result = "Not found"
+        }
+        return result
     }
 }
